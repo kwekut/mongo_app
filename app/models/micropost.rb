f13abfb73cd6b41df911f20959748a797aa04fc2
kwekut <your.kwekut@gmail.com>
2014-05-13 20:26:25 -0400

    Finish demo app

---
+++ app/models/micropost.rb
@@ -0,0 +1,10 @@
+class Micropost
+  include MongoMapper::Document
+
+  key :content, String
+  key :user_id, Integer
+
+  belongs_to :user
+
+
+end
---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User
+  include MongoMapper::Document
+
+  key :name, String
+  key :email, String
+
+  many :microposts
+
+end
+
